1c730078536b01647045e01ff42b32626a5aba11
Wahiduzzaman Siddique <wahid2272@gmail.com>
2023-02-13 13:51:30 +0200

    github yml env setting testing

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,23 +1,13 @@
 terraform {
-  required_version = ">=1.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
+      version = "=3.0.0"
     }
   }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {
-    subscription_id = var.SUBSCRIPTION_ID
-    tenant_id       = var.TENANT_ID 
-    client_id       = var.CLIENT_ID 
-    client_secret   = var.CLIENT_SECRET
-  }
+  features {}
 }
